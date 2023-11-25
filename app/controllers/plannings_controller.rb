ef5d531f714b83c91bb79eeb0800ee17028cc3da
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 16:49:16 -0400

    feat(views, controllers, helpers): add crud plannnings

--- app/controllers/plannings_controller.rb
+++ app/controllers/plannings_controller.rb
@@ -1,5 +1,62 @@
 class PlanningsController < ApplicationController
+  before_action :set_planning, only: [ :edit, :update, :destroy ]
+  
   def index
     @plannings = Planning.all
+  end
+
+  def show
+    @planning = Planning.includes(:expense_categories, :expenses, :incomes).find(params[:id])
+  end
+
+  def new
+    @planning = Planning.new
+  end
+
+  def create
+    @planning = Planning.new(planning_params)
+
+    respond_to do |format|
+      if @planning.save
+        format.html { redirect_to plannings_path, notice: "Planejamento cadastrado com sucesso." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @planning.errors, status: :unprocessable_entity }
+        format.turbo_stream { render :form_update, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @planning.update(planning_params)
+        format.html { redirect_to plannings_path, notice: "Planejamento atualizado com sucesso." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @planning.errors, status: :unprocessable_entity }
+        format.turbo_stream { render :form_update, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    if @planning.destroy
+      redirect_to plannings_path, notice: "Planejamento excluído com sucesso."
+    else
+      redirect_to plannings_path, notice: "Ocorreu um erro. Tente novamente ou contate o suporte."
+    end
+  end
+
+  private
+
+  def set_planning
+    @planning = Planning.find(params[:id])
+  end
+
+  def planning_params
+    params.require(:planning).permit(:month_year).merge(user_id: 1)
   end
 end
