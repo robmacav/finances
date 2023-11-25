a36bd1d4270d0109fbcf6d20c7567f35ad1f5327
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 14:18:42 -0400

    build(config, javascript): add Bootstrap v5.1.3

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
