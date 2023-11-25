e454de9b22ccecd2e7e14e4994d4b0be9d82cbcb
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 16:52:21 -0400

    feat(config, views, controllers): add crud planning expenses

---
+++ app/controllers/planning_expenses_controller.rb
@@ -0,0 +1,56 @@
+class PlanningExpensesController < ApplicationController
+    before_action :set_expense, only: [ :update, :edit, :destroy ]
+  
+    def new
+      @category = PlanningExpenseCategory.find(params[:category_id])
+      @expense = PlanningExpense.new(planning_expense_category_id: @category.id)
+    end
+  
+    def create
+      @expense = PlanningExpense.new(expense_params)
+  
+      respond_to do |format|
+        if @expense.save
+          format.html { redirect_to planning_expenses_path, notice: "Despesa cadastrada com sucesso." }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @expense.errors, status: :unprocessable_entity }
+          format.turbo_stream { render :form_update, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      respond_to do |format|
+        if @expense.update(expense_params)
+          format.html { redirect_to planning_expenses_path, notice: "Despesa atualizado com sucesso." }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @expense.errors, status: :unprocessable_entity }
+          format.turbo_stream { render :form_update, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    def destroy
+      if @expense.destroy
+        redirect_to planning_expenses_path, notice: "Despesa excluído com sucesso."
+      else
+        redirect_to planning_expenses_path, notice: "Ocorreu um erro. Tente novamente ou contate o suporte."
+      end
+    end
+  
+    private
+  
+    def set_expense
+      @expense = PlanningExpense.find(params[:id])
+    end
+  
+    def expense_params
+      params.require(:planning_expense).permit(:description, :value, :quantity, :planning_expense_category_id)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :plannings
   resources :planning_incomes, path: 'planning-incomes'
+  resources :planning_expenses, path: 'planning-expenses'
   resources :planning_expense_categories, path: 'planning-expense-categories'
   
   root to: redirect('/plannings')
