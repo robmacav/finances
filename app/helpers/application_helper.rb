7ada0fe88a067d5822f7301e9b240126144003c2
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 18:10:41 -0400

    feat(config, views, controllers, helpers): configure pagy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+    include Pagy::Frontend
+    
     def decimal_to_float(value)
         integer, decimal = format('%.2f', value).split('.')
         integer.reverse.gsub(/(\d{3})(?=\d)/, '\1.').reverse + ',' + decimal
