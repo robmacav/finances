bfaefed6765e56c85f4d1fddab5bd5425605bca0
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 22:11:19 -0400

    feat(controllers): validate login session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
 
+    before_action :authenticate_user!
+
     def convert_float_to_decimal(value)
         value&.gsub(".", "")&.sub(",",".")
     end
