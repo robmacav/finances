afe53857abd42b948aaa137930271aec688fc630
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 22:04:50 -0400

    feat(config): add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :plannings
   resources :planning_incomes, path: 'planning-incomes'
   resources :planning_expenses, path: 'planning-expenses'
