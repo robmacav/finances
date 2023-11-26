8a55aab37efbd03f15742de466ec04e7557f717d
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 21:30:25 -0400

    feat(models): add method convert decimal value to float

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,13 @@ class ApplicationRecord < ActiveRecord::Base
   def self.ransackable_attributes(auth_object = nil)
     ["month_year"]
   end
+
+  def value_decimal_to_float
+    formatted = format('%.2f', self.value).tr('.', ',')
+
+    parts = formatted.split(',')
+    parts[0] = parts[0].reverse.scan(/\d{1,3}/).join('.').reverse
+
+    parts.join(',')
+  end
 end
