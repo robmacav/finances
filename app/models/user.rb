cd42af1ac7f478efe31a65962e22aa74298152ca
Robson Cavalcante <robmacav@gmail.com>
2023-11-26 12:22:00 -0400

    feat(models): add fullname user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :plannings, dependent: :destroy
     
     validates_presence_of :first_name, :last_name, :email
+
+    def fullname
+        "#{self.first_name} #{self.last_name}"
+    end
 end
