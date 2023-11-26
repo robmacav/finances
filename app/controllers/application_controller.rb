3dceb9716c44f8f53ccb5b2e6a8d875adf67d419
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 22:12:01 -0400

    feat(controllers): permit first_name and last_name attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
     include Pagy::Backend
 
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     def convert_float_to_decimal(value)
         value&.gsub(".", "")&.sub(",",".")
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     end
 end
