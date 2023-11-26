f16cee84f02dc53ca3a1121cdbd52766185d965e
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 20:56:30 -0400

    refactor(controllers): change redirect after actions

--- app/controllers/planning_incomes_controller.rb
+++ app/controllers/planning_incomes_controller.rb
@@ -1,5 +1,6 @@
 class PlanningIncomesController < ApplicationController
     before_action :set_income, only: [ :update, :edit, :destroy ]
+    before_action :normalization_params, only: [ :create, :update ]
 
     def index
       @incomes = PlanningIncome.where(planning_id: params[:planning_id])
@@ -14,7 +15,7 @@ class PlanningIncomesController < ApplicationController
   
       respond_to do |format|
         if @income.save
-          format.html { redirect_to planning_incomes_path(planning_id: @income.planning_id), notice: "Receita cadastrada com sucesso." }
+          format.html { redirect_to planning_path(@income.planning_id), notice: "Receita cadastrada com sucesso." }
         else
           format.html { render :new, status: :unprocessable_entity }
           format.json { render json: @income.errors, status: :unprocessable_entity }
@@ -29,7 +30,7 @@ class PlanningIncomesController < ApplicationController
     def update
       respond_to do |format|
         if @income.update(income_params)
-          format.html { redirect_to planning_incomes_path(planning_id: @income.planning_id), notice: "Receita atualizada com sucesso." }
+          format.html { redirect_to planning_path(@income.planning_id), notice: "Receita atualizada com sucesso." }
         else
           format.html { render :new, status: :unprocessable_entity }
           format.json { render json: @income.errors, status: :unprocessable_entity }
@@ -40,16 +41,20 @@ class PlanningIncomesController < ApplicationController
   
     def destroy
       if @income.destroy
-        redirect_to planning_incomes_path(planning_id: @income.planning_id), notice: "Receita excluída com sucesso."
+        redirect_to planning_path(@income.planning_id), notice: "Receita excluída com sucesso."
       else
-        redirect_to planning_incomes_path(planning_id: @income.planning_id), notice: "Ocorreu um erro. Tente novamente ou contate o suporte."
+        redirect_to planning_path(@income.planning_id), notice: "Ocorreu um erro. Tente novamente ou contate o suporte."
       end
     end
   
     private
 
+    def normalization_params
+      params[:planning_income][:value] = convert_float_to_decimal(params[:planning_income][:value])
+    end
+
     def set_income
-      @income = PlanningIncome.find(params[:id].to_i)
+      @income = PlanningIncome.find(params[:id])
     end
   
     def income_params
