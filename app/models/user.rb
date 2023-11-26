2e2ef70ebb6b5ab0a6cff5c8098a56e57a251fe7
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 22:04:25 -0400

    feat(db, models): add devise to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
     has_many :plannings, dependent: :destroy
     
     validates_presence_of :first_name, :last_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_25_162646) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_26_020014) do
   create_table "planning_expense_categories", force: :cascade do |t|
     t.string "description", null: false
     t.string "color", limit: 6, null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_25_162646) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "planning_expense_categories", "plannings"
