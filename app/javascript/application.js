a6e404d96fcf62abdc4ef0e9566972769e658d49
Robson Cavalcante <robmacav@gmail.com>
2023-11-25 21:38:16 -0400

    build(config, javascript): add Datepicker v1.10.0

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require currency
+//= require datepicker
 
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import "jquery"
 import "jquery-mask"
+import "datepicker"
