1c1cbb084bacfcc8107b23c1c1824b4367b3e7a7
Robson Cavalcante <robmacav@gmail.com>
2023-12-04 23:44:10 -0400

    feat(models): add scope sum all revenues by current month year

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -5,6 +5,8 @@ class Revenue < ApplicationRecord
 
   scope :current_month_year, -> { where("date like ?", "%#{Time.now.strftime("%m%Y")}%") }
 
+  scope :all_by_current_month_year_sum, -> { where("date like ?", "%#{Date.today.strftime("%m%Y")}%").sum(:value) }
+
   def self.ransackable_associations(auth_object = nil)
     ["user"]
   end
