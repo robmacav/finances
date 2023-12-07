74772f13a0f139b8020b6e27342805ec08411aee
Robson Cavalcante <robmacav@gmail.com>
2023-12-07 15:06:51 -0400

    fix(models): validate email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
     has_many :plannings, dependent: :destroy
     
-    validates_presence_of :first_name, :last_name, :email
+    validates_presence_of :first_name, :last_name
+    validates_presence_of :email, uniqueness: true
 
     enum status: { 
         'Ativo': nil, 
