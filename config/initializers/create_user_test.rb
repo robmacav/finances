f23bbe6cb1821e2590365260a99915dfbc184f09
Robson Machado Cavalcante <robson.cavalcante@sefin.ro.gov.br>
2024-10-25 10:00:49 -0400

    feat(config): add script create user test

---
+++ config/initializers/create_user_test.rb
@@ -0,0 +1,9 @@
+Rails.application.config.after_initialize do
+    if Rails.env.development?
+        User.find_or_create_by(email: 'explorer@finances.me') do |user|
+        user.first_name = 'Sr.'
+        user.last_name = 'Finances'
+        user.password = '111111'
+        end
+    end
+end
