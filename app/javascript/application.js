e5101e9cfef8a31350108918149b84be8b608f2a
Robson Machado Cavalcante <robson.cavalcante@sefin.ro.gov.br>
2024-10-30 07:57:00 -0400

    feat(config, views, assets, javascript): set layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,4 @@
 import "@hotwired/turbo-rails"
+import "controllers"
+import "bootstrap"
+import "jquery"
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.1/jquery.min.js"
