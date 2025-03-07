979e2f9c8a4eb296eac5a930db102fd387c1bc00
Robson M. Cavalcante <robson.cavalcante@sefin.ro.gov.br>
2025-03-07 15:26:01 -0400

    Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts.clear
+  config.hosts << 'desenv23.dev.sefin.ro.gov.br'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
