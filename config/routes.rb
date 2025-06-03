4c1088f5a37bb42596363c352a4737e5b629d579
Robson M. Cavalcante <robmacav@gmail.com>
2025-06-03 10:26:43 -0400

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    resources :incomes
+    resouces :expenses
+    resources :categories
+  end
+
+  namespace :web do
+    resources :incomes
+    resources :expenses
+    resources :categories
+  end
 end
