56f625bfbf664312a6c8748861d68b63f37f2676
Robson M. Cavalcante <robmacav@gmail.com>
2025-06-09 13:08:41 -0400

    feat(api): add incomes crud

---
+++ api/db/migrate/20250609150951_create_incomes.rb
@@ -0,0 +1,14 @@
+class CreateIncomes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :incomes do |t|
+      t.string :summary
+      t.text :details
+      t.decimal :value
+      t.string :date, limit: 8
+      t.integer :category_id, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
+  end
+end
