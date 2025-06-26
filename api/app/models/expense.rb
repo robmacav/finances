2e607a3f6ac9be842e4d7bf0a78c536686692785
Robson M. Cavalcante <robson.cavalcante@sefin.ro.gov.br>
2025-06-26 11:03:04 -0400

    db, json api

--- api/app/models/expense.rb
+++ api/app/models/expense.rb
@@ -4,4 +4,28 @@ class Expense < ApplicationRecord
     belongs_to :status, class_name: 'Status', foreign_key: 'status_id'
     belongs_to :user, class_name: 'User', foreign_key: 'user_id'
     belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
+
+    def as_json(options = {})
+    {
+        id: id,
+        summary: summary,
+        details: details,
+        value: value,
+        date: date,
+        status: {
+            id: status&.id,
+            summary: status&.summary
+        },
+        category: {
+            id: category.id,
+            summary: category.summary,
+            color: category.color
+        },
+        user: {
+            id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name
+        }
+        }
+    end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :v1 do 
+    resources :incomes
+    resources :expenses
+    resources :categories
+  end
 end
