d59c8de517f72c963f116f4b12f4fd6f9b493bd2
Robson M. Cavalcante <robmacav@gmail.com>
2025-10-11 23:38:52 -0400

    feat(web): set app component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
+
+import Index from "./app/pages/dashboard/Index";
 
 function App() {
   return (
-    <></>
-  )
+    <>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Index />} />
+          </Routes>
+      </Router>
+
+      <Toaster position="top-right" />
+    </>
+  );
 }
 
-export default App
+export default App;
