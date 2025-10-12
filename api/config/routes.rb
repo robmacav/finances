9814b0d5763aa161be04dd43e8cc7fa2d7f1eb59
Robson M. Cavalcante <robmacav@gmail.com>
2025-10-11 22:27:49 -0400

    feat(api): add transactions utils form params endpoint

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
     namespace :reports do
       get "transactions/all-by-month-year", to: "transactions#all_by_month_year"
     end
+
+    namespace :utils do
+      get "transactions/form-data", to: "transactions#form_data"
+    end
   end
 end
