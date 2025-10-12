e6656c0b53bd2a30f0f55c19a179bf3fcddf8e68
Robson M. Cavalcante <robmacav@gmail.com>
2025-10-11 21:59:50 -0400

    feat(api): add scope methods filters and transaction reports

--- api/app/models/transaction.rb
+++ api/app/models/transaction.rb
@@ -6,4 +6,80 @@ class Transaction < ApplicationRecord
     belongs_to :category, class_name: 'Category', foreign_key: 'category_id', optional: true
 
     enum :kind, { expense: 1, income: 2 }
+
+    scope :by_month_year, ->(month_year, status_name = nil) {
+        includes(:category, :status)
+        .where(date: date_range_by_month_year_string(month_year))
+        .order(date: :asc, summary: :asc)
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+    }
+
+    scope :by_kind_and_month_year, ->(kind, month_year, status_name = nil) {
+        includes(:category, :status)
+        .where(kind: kind, date: date_range_by_month_year_string(month_year))
+        .order(date: :asc, summary: :asc)
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+    }
+
+    scope :sum_by_kind_and_month_year, ->(kind, month_year, status_name = nil) {
+        where(kind: kind, date: date_range_by_month_year_string(month_year))
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+        .sum(:value)
+    }
+
+    scope :by_kind_and_month_year_per_category, ->(kind, month_year, status_name = nil) {
+        where(kind: kind, date: date_range_by_month_year_string(month_year))
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+        .select("category_id, SUM(value) as total")
+        .group("category_id")
+        .order("total desc")
+        .includes(:category)
+    }
+
+    scope :total_months_by_kind_per_year, ->(kind, year, status_name = nil) {
+        where(kind: kind, date: Date.new(year).beginning_of_year..Date.new(year).end_of_year)
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+        .select("TO_CHAR(date, 'TMMon') AS month, SUM(value) AS total")
+        .group("month")
+        .order("MIN(date)")
+    }
+
+    scope :by_month_year_expenses_most_frequents, ->(month_year, status_name = nil) {
+    where(kind: :expense, date: date_range_by_month_year_string(month_year))
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+        .select("transactions.summary, COUNT(*) AS qtd, SUM(value) AS total")
+        .group("transactions.summary")
+        .order("qtd DESC")
+    }
+
+    scope :expenses_on_current_week_total_by_day, ->(status_name = nil) {
+        start_date = Date.today.beginning_of_week(:monday)
+        end_date = Date.today.end_of_week(:sunday)
+
+        select("TO_CHAR(date, 'Day') AS day_name, SUM(value) AS total")
+        .where(kind: :expense, date: start_date..end_date)
+        .then { |q| status_name ? q.joins(:status).where(statuses: { summary: status_name }) : q }
+        .group("day_name")
+        .order(Arel.sql("MIN(date)"))
+    }
+
+    def format_brazilian_currency
+        whole, decimal = sprintf('%.2f', value).split(".")
+
+        whole_with_delimiter = whole.reverse.gsub(/(\d{3})(?=\d)/, '\\1.').reverse
+       
+        "#{whole_with_delimiter},#{decimal}"
+    end
+
+    private
+
+    def self.date_range_by_month_year_string(month_year)
+        formatted = "#{month_year[0..1]}/#{month_year[2..5]}"
+
+        start_date = Date.strptime(formatted, "%m/%Y")
+
+        end_date = start_date.end_of_month
+
+        start_date..end_date
+    end
 end
